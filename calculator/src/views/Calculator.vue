531db0cf19866b3adb7a13a353feb1954569946f
Gogotchuri <igogo17@freeuni.edu.ge>
2020-11-20 12:15:34 +0400

    Assignment project created

---
+++ calculator/src/views/Calculator.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+
+    <v-expansion-panels>
+      <v-expansion-panel>
+        <v-expansion-panel-header>
+          Calculator title here?
+        </v-expansion-panel-header>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Calculator"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ calculator/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row class="px-15">
+      <calculator/>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Calculator from "@/views/Calculator";
+export default {
+  name: "Home",
+  components: {Calculator}
+}
+</script>
+
+<style scoped>
+
+</style>
